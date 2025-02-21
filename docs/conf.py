6de62f2dab2b64e20470c15060f171759f04ad3a
EB96 <ehsan.bhatti.ai@gmail.com>
2025-02-21 02:02:48 +0000

    Editing docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ autodoc_mock_imports = [
         'pandas',
         'numpy',
         'scipy',
-        'scikit-learn',
+        'scikit-learn', 'sklearn',
         'drugstone',
         'goatools',
         'mygene',
